d90083a15beea10a828ffd2487e32c2488b11013
DrBCUI <32187298+DrBCUI@users.noreply.github.com>
2020-01-20 13:55:58 -0800

    Add files via upload

---
+++ Ch1/helloWorld.py
@@ -0,0 +1,6 @@
+# Written by: Julie Beeston
+# This program prints “Hello World”.
+# To run this code, open a terminal window and type:
+#            python.exe helloWorld.py
+
+print("Hello World!") # Command to print "Hello World!"
