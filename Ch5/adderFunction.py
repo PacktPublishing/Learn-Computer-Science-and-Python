3260e12ba1303c5fa1b9f51693f46356d73a7443
DrBCUI <32187298+DrBCUI@users.noreply.github.com>
2020-11-26 20:05:22 -0800

    Create adderFunction.py

---
+++ Ch5/adderFunction.py
@@ -0,0 +1,12 @@
+import sys
+
+def adder(x,y): return (x+y)
+
+if(len(sys.argv) == 3): 
+   x = int(sys.argv[1])
+   y = int(sys.argv[2])
+   z = adder(x,  y)
+   print (x, " + ", y, " = ", z )
+else:
+   print ("You need to include the 2 numbers you want me to add. For example:")
+   print ("python.py " + sys.argv[0], " 4 5")
